d541df3d843656ce6c064cdb656a53b03dbd2487
49257620 <49257620@qq.com>
2018-03-21 12:33:38 +0800

    题目：文本颜色设置

---
+++ studysrc/example/exam035.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+"""
+【程序35】
+题目：文本颜色设置
+1.程序分析：
+2.程序源代码：
+3.参考颜色：https://www.cnblogs.com/ping-y/p/5897018.html
+"""
+
+print('This is a \033[1;35m test \033[0m!')
+print('This is a \033[1;32;43m test \033[0m!')
+print('\033[1;33;44mThis is a test !\033[0m')

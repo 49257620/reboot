af1fbd91aec9bc0646a7b1598aafacfdda2759a4
49257620 <49257620@qq.com>
2018-03-21 12:22:41 +0800

    题目：print改变字体颜色等

--- studysrc/example/exam032.py
+++ studysrc/example/exam032.py
@@ -9,7 +9,7 @@
 # from termcolor import *
 # print(colored("Error: this is an error.","red"))
 # print(colored("Warning: this is a warning.","yellow"))
-
+#
 print('This is a \033[1;35m test \033[0m!')
 print('This is a \033[1;32;43m test \033[0m!')
 print('\033[1;33;44mThis is a test !\033[0m')

adc4f67d910f1264abd6e5f5b672b3876a002ba2
49257620 <49257620@qq.com>
2018-03-22 08:57:16 +0800

    题目：互相赋值

---
+++ studysrc/example/exam047.py
@@ -0,0 +1,28 @@
+# encoding: utf-8
+"""
+【程序46】
+题目：互相赋值　
+1.程序分析：
+2.程序源代码：
+
+"""
+
+a = 1
+b = 2
+c = 3
+print(a, b)
+a, b = b, a
+
+a = 1
+b = 2
+c = 3
+print(a, b, c)
+a, b, c = b, c, a
+print(a, b, c)
+
+a = 1
+b = 2
+c = 3
+print(a, b, c)
+a, b, c = a + b, b + c, c + a
+print(a, b, c)

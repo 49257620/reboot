63aeb70bbbc488d46210c4c58e00bf3ae1b8b518
49257620 <49257620@qq.com>
2018-03-23 12:20:14 +0800

    题目：时间函数举例

--- studysrc/example/exam091.py
+++ studysrc/example/exam091.py
@@ -11,3 +11,12 @@ import time
 print(time.ctime(time.time()))
 print(time.asctime(time.localtime(time.time())))
 print(time.asctime(time.gmtime(time.time())))
+
+
+start = time.time()
+for i in range(5):
+    print(i+1)
+    time.sleep(1)
+    end = time.time()
+
+print(end - start)

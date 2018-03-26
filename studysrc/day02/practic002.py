07db5edfecfa2b12c50b8c545794972a815b33a1
49257620 <49257620@qq.com>
2018-03-26 13:09:02 +0800

    day02 用户管理实现

--- studysrc/day02/practic002.py
+++ studysrc/day02/practic002.py
@@ -1,3 +1,25 @@
 # encoding: utf-8
 # author = ‘LW’
 
+users = []
+
+users.append((1, 'Liu Wei', 29, '18500000001'))
+users.append((2, 'Li Si', 28, '18600000002'))
+users.append((3, 'Zhang S', 30, '18700000003'))
+
+
+find_user = None
+find_user_list = list((x for x in users if x[0]==1))
+
+print(find_user_list)
+
+if(len(find_user_list)>0):
+    find_user =  find_user_list[0]
+
+
+edit_info = 'lw,40,123'
+print((find_user[0],)+tuple(edit_info.split(',')))
+users[users.index(find_user)] = (find_user[0],)+tuple(edit_info.split(','))
+
+
+print(users)

3aaf2cd823bfae94ae15bb8394367cbc4a5cc05d
49257620 <49257620@qq.com>
2018-04-03 16:43:12 +0800

    day03 用户管理实现，增加了排序功能

--- homework/day03/user_mgr_tmp.py
+++ homework/day03/user_mgr_tmp.py
@@ -3,10 +3,10 @@
 
 # 初始化用户清单
 users = {
-    1: {'name': 'kk1', 'age': 28, 'tel': '158xxxxx'},
+    4: {'name': 'kk1', 'age': 28, 'tel': '158xxxxx'},
     2: {'name': 'kk15', 'age': 29, 'tel': '168xxxxx'},
     3: {'name': 'kk3', 'age': 30, 'tel': '178xxxxx'},
-    4: {'name': 'kk4', 'age': 31, 'tel': '158xxxxx'}
+    1: {'name': 'kk4', 'age': 31, 'tel': '158xxxxx'}
 }
 
 title = ('Id', 'Name', 'Age', 'Tel')
@@ -43,3 +43,12 @@ def search_user(conditions):
 
 
 print (search_user('15'))
+
+users_list  = []
+for k,v in users.items():
+    users_tuple = (k,v.get('name'),v.get('age'),v.get('tel'))
+    users_list.append(users_tuple)
+
+print(sorted(users_list, key=lambda s: s[0]))
+print(users_list)
+print(list(users.values()))

868d56f2f89276dd8b1f53888b91a9ae123f7374
49257620 <49257620@qq.com>
2018-04-03 15:27:38 +0800

    day03 作业用户管理实现

---
+++ homework/day03/user_mgr_tmp.py
@@ -0,0 +1,45 @@
+# encoding: utf-8
+# Author: LW
+
+# 初始化用户清单
+users = {
+    1: {'name': 'kk1', 'age': 28, 'tel': '158xxxxx'},
+    2: {'name': 'kk15', 'age': 29, 'tel': '168xxxxx'},
+    3: {'name': 'kk3', 'age': 30, 'tel': '178xxxxx'},
+    4: {'name': 'kk4', 'age': 31, 'tel': '158xxxxx'}
+}
+
+title = ('Id', 'Name', 'Age', 'Tel')
+format_title = '|{0[0]:^10s}|{0[1]:^20s}|{0[2]:^10s}|{0[3]:^24s}|'
+title_str = format_title.format(title)
+
+format_str = '|{0[0]:^10d}|{0[1][name]:^20s}|{0[1][age]:^10d}|{0[1][tel]:^24s}|'
+
+print((1,users.get(1)))
+
+user_temp = {'name': 'kk5', 'age': 31, 'tel': '158xxxxx'}
+
+print(users)
+
+def check_name_exist(id, name):
+    is_exist = False
+    for key , user in users.items() :
+        if key == id:
+            continue
+        else:
+            if user.get('name') == name:
+                is_exist = True
+                break
+    return is_exist
+
+
+def search_user(conditions):
+    search_users_list  = {}
+    for key , user in users.items() :
+        if user.get('name').find(conditions.strip()) != -1 or user.get('tel').find(conditions.strip()) != -1:
+            search_users_list[key] =  user
+
+    return search_users_list
+
+
+print (search_user('15'))

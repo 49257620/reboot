2afd42becbc8fb9ea86df61a4f3d8ca0330ddd1a
49257620 <49257620@qq.com>
2018-06-02 16:42:46 +0800

    mysql create db

---
+++ studysrc/install/create_mysql_db_utf_8.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+# -*- coding: utf-8 -*-
+# author = ‘LW’
+
+
+'''
+mysql创建utf-8字符集数据库
+
+CREATE DATABASE lwdb DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+
+更改数据库的字符编码
+ALTER DATABASE db_name DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+'''
